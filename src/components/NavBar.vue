6486c5d362325264aae7d1f06e49133fdff30cda
Andres Colombon <acolomb2@stu.southtexascollege.edu>
2023-09-27 17:14:31 -0500

    added navigation layout

---
+++ src/components/NavBar.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { ref } from 'vue'
+const brand = ref('🏢 Fake Company Directory')
+</script>
+
+<template>
+    <nav>
+        <div class="wrapper">
+            <div class="brand">
+                <span class="brand-title">{{ brand }}</span>
+            </div>
+            <div class="menu">
+                <a href="#" class="menu-item">Departments</a>
+                <a href="#" class="menu-item">Settings</a>
+                <a href="#" class="menu-login">Logout</a>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<style scoped lang="postcss">
+    nav{
+        @apply flex h-20 bg-slate-900 text-slate-200;
+
+        .wrapper{
+            @apply container mx-auto flex w-full items-center justify-between;
+            .brand{
+                &-title{
+                    @apply text-2xl font-bold text-yellow-500;
+                }
+            }
+
+            .menu{
+                @apply flex gap-2;
+                &-item{
+                    @apply rounded-md px-4 py-2 hover:bg-yellow-500 hover:text-slate-900;
+                }
+                &-login{
+                    @apply rounded-md px-4 py-2 bg-red-500 hover:bg-red-700 text-red-100;
+                }
+            }
+        }
+    }
+
+    
+</style>
